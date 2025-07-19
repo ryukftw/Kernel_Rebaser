1b42c6a64763953dd9e158f50502ad027cc5e944
Bijoy Sarkar <workforbijoy9@gmail.com>
2025-07-19 03:37:44 +0000

    okverynice

--- Kernel_Updater.py
+++ Kernel_Updater.py
@@ -2,10 +2,10 @@
 import os
 
 # Variables - Changable
-KERNEL_VERSION = "4.14" # Set Kernel Version (4.4/4.14/4.19)
-KERNEL_TAG = "LA.UM.9.1.r1-14700-SMxxx0.QSSI13.0" # Set CAF Tag / Upstream Version (LA.UM.10.2.1.r1-0300-sdm660.0/v4.19.157)
-REPO_LINK = "https://github.com/builder1206/msm-4.14" # Repo link to pull/fetch/push Kernel
-BASE_BRANCH = "base" # Base branch to pick the old/device base changes from
+KERNEL_VERSION = "4.19" # Set Kernel Version (4.4/4.14/4.19)
+KERNEL_TAG = "LA.UM.9.15.2.r1-11500-KAMORTA.QSSI14.0" # Set CAF Tag / Upstream Version (LA.UM.10.2.1.r1-0300-sdm660.0/v4.19.157)
+REPO_LINK = "https://github.com/ryukftw/kernel_realme_RMX3371" # Repo link to pull/fetch/push Kernel
+BASE_BRANCH = "phoeniX-AOSP" # Base branch to pick the old/device base changes from
 
 # Variables - Non_Changable
 QCACLD_LINK = "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0" # Qcacld repo link
@@ -13,7 +13,6 @@ FW_API_LINK = "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource
 QCA_WIFI_HOST_CM_LINK = "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn" # Qualcom Wifi host repo link
 AUDIO_TECHPACK_LINK = "https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel" # Audio Techpack repo link
 DATA_TECHPACK_LINK = "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/data-kernel" # data
-EXFAT_LINK = "https://github.com/arter97/exfat-linux" # Exfat repo link
 
 # Clone the kernel
 os.system("git clone https://git.codelinaro.org/clo/la/kernel/msm-%s -b %s %s"%(KERNEL_VERSION,KERNEL_TAG,KERNEL_TAG))
@@ -38,9 +37,6 @@ os.system("git subtree add --prefix=techpack/audio %s %s --squash"%(AUDIO_TECHPA
 
 # Add Data Techpack
 os.system("git subtree add --prefix=techpack/data %s %s --squash"%(DATA_TECHPACK_LINK,KERNEL_TAG))
-
-# Add Exfat
-os.system("git subtree add --prefix=fs/exfat %s master --squash"%(EXFAT_LINK))
 
 # Push Changes to Repo
 os.system("git remote add upstream  %s"%(REPO_LINK))
